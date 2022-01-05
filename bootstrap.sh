1c599d5d08490e7ee07fae10715628897f0d7377
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-05 10:31:12 +0900

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,5 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin main;
-
-function doIt() {
-	rsync
-        # --exclude ".git/" \
-		# --exclude ".DS_Store" \
-		# --exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		# --exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.zprofile;
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zsh_history ~/.zsh_history
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.config/git/ignore ~/.config/git/ignore

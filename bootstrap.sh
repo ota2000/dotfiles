bdcb0183b0e991a8cbf90e852cb7a41090dc9361
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-05 11:25:55 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zsh_history ~/.zsh_history
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.config/git/ignore ~/.config/git/ignore
 ln -s ~/dotfiles/.aliases ~/.config/git/ignore

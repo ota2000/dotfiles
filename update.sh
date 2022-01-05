88fe35298cc64865d7925657e6bffce31c94cf3e
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-05 09:29:22 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,4 +3,7 @@ DOT_DIR=$HOME/dotfiles
 git -C ${DOT_DIR} pull origin main
 
 # link
-./ln.sh
+ln -sf $DOT_DIR/.zshrc ~/.zshrc
+ln -sf $DOT_DIR/.zsh_history ~/.zsh_history
+ln -sf $DOT_DIR/.gitconfig ~/.gitconfig
+ln -sf $DOT_DIR/.config/git/ignore ~/.config/git/ignore

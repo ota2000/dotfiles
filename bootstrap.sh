b99923413e88df32e17e2de1f95c201583f9103b
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-05 11:37:02 +0900

    update .macos

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-ln -s ~/dotfiles/.zprofile ~/.zprofile
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.config/git/ignore ~/.config/git/ignore
-ln -s ~/dotfiles/.aliases ~/.config/git/ignore

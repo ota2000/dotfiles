c0817ad6717fa584c57440336e5c66194e3e51e2
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-05 09:02:45 +0900

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# installation
+
+`bash -c "$(curl -fsSL https://raw.githubusercontent.com/OTA2000/dotfiles/main/install.sh)"`
--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ git clone https://github.com/OTA2000/dotfiles.git ${DOT_DIR}
 # link
 ln -sf $DOT_DIR/.zshrc ~/.zshrc
 ln -sf $DOT_DIR/.gitconfig ~/.gitconfig
-ln -sf $DOT_DIR/.gitignore ~/.config/git/ignore
+ln -sf $DOT_DIR/.config/git/ignore ~/.config/git/ignore

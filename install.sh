654dd4358407dd42cf41a5c4f674bdd4dc552efc
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-05 09:28:54 +0900

    updated

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ DOT_DIR=$HOME/dotfiles
 git clone https://github.com/OTA2000/dotfiles.git ${DOT_DIR}
 
 # link
-sh ./ln.sh
+./ln.sh
--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ DOT_DIR=$HOME/dotfiles
 git -C ${DOT_DIR} pull origin main
 
 # link
-sh ./ln.sh
+./ln.sh

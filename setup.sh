a42127784104d604f54b72b1a4a4ba1b44b0442a
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-07 15:50:02 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -60,6 +60,51 @@ brew bundle cleanup --global --force
 
 echo "--- Brew bundle is Done!  ---"
 
+echo "--- asdf setting is Start! ---"
+log() {
+    message=$1
+    echo "$message"
+}
+is_dir() {
+    path=$1
+    [ -d "$path" ]
+}
+
+for plugin in $(awk '{print $1}' ~/.tool-versions); do
+    if ! is_dir ~/.asdf/plugins/"$plugin"; then
+        asdf plugin add "$plugin"
+    fi
+done
+
+is_runtime_versions_changed() {
+    plugin="$1"
+    specified=$(grep "$plugin" ~/.tool-versions | awk '
+    {$1=""; print $0}')
+
+    installed=$(asdf list "$plugin" 2>&1)
+
+    is_changed=
+    for version in $specified; do
+        match=$(echo "$installed" | grep "$version")
+        [ -z "$match" ] && is_changed=1
+    done
+
+    [ "$is_changed" ]
+}
+
+for plugin in $(asdf plugin list); do
+    if is_runtime_versions_changed "$plugin"; then
+        if [ "$plugin" = nodejs ]; then
+            log "Import release team keyring for Node.JS"
+            bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-previous-release-team-keyring'
+        fi
+        log "Install runtime: $plugin"
+        asdf install "$plugin"
+    fi
+done
+
+echo "--- asdf setting is Done!  ---"
+
 if ask 'macOS setup?'; then
     echo "--- Setup macOS is Start! ---"
     source ~/dotfiles/.macos

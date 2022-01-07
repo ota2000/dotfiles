daa98afd4ac1ac8f580609f24d9645ca6a9a20a3
OTA2000 <yas.arakaki+github@gmail.com>
2022-01-07 11:23:48 +0900

    update dotfiles

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -10,7 +10,7 @@ module.exports = {
         // default font size in pixels for all tabs
         fontSize: 13,
         // font family with optional fallbacks
-        fontFamily: 'MesloLGS NF',
+        fontFamily: 'Ricty Diminished',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
